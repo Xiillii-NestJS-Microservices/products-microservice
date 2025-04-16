10d4203bfea0770e212a0d8291c2fd41990e102f
Jose Alonso <mail@josealonso.dev>
2025-04-16 13:07:50 -0600

    Paginate the product list with Prisma
    Page and Limit, and improve the response with pagination metadata

--- src/products/products.service.ts
+++ src/products/products.service.ts
@@ -2,6 +2,7 @@ import { Injectable, Logger, OnModuleInit } from '@nestjs/common';
 import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { PrismaClient } from 'generated/prisma';
+import { PaginationDto } from 'src/common';
 
 @Injectable()
 export class ProductsService extends PrismaClient implements OnModuleInit {
@@ -18,8 +19,23 @@ export class ProductsService extends PrismaClient implements OnModuleInit {
     });
   }
 
-  findAll() {
-    return this.product.findMany({});
+  async findAll(paginationDto: PaginationDto) {
+    const { page, limit } = paginationDto;
+
+    const totalProducts = await this.product.count();
+    const lastPage = Math.ceil(totalProducts / (limit ?? 1));
+
+    return {
+      data: await this.product.findMany({
+        skip: (page ?? 1 - 1) * (limit ?? 1),
+        take: limit,
+      }),
+      meta: {
+        total: totalProducts,
+        page: page,
+        lastPage: lastPage,
+      },
+    };
   }
 
   findOne(id: number) {
